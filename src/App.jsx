f974309cd31e28dd984e18ba658e1fddcd5fcd3e
JHM92 <jake.murdoch@live.com>
2025-11-18 22:43:01 +0000

    Created <CheckboxOption> component which has a prompt and option attribute and creates a checkbox and label. Checkbox name and id are the prompt and option combined, checkbox value and label contents is the option with a capitalised first letter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
+import CheckboxOption from "./components/CheckboxOption";
 
 function App() {
   return (
     <>
       <Header />
+
+      <div className="property-types-wrapper">
+        Property Types
+        <div className="property-type-options">
+          <CheckboxOption prompt="property-type" option="house" />
+          <CheckboxOption prompt="property-type" option="apartment" />
+          <CheckboxOption prompt="property-type" option="studio" />
+        </div>
+      </div>
     </>
   );
 }
---
+++ src/components/CheckboxOption.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function CheckboxOption({ prompt, option }) {
+  const optionName = prompt + "-" + option;
+
+  const optionValue = option[0].toUpperCase() + option.slice(1).toLowerCase();
+
+  return (
+    <div className="checkbox-option">
+      <input type="checkbox" name={optionName} id={optionName} value={optionValue} />
+      <label htmlFor={optionName}>{optionValue}</label>
+    </div>
+  );
+}
