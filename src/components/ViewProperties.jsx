bbd0635e2ff8d7471164c3a7441ac6f25f2f954c
JHM92 <jake.murdoch@live.com>
2025-11-20 00:43:38 +0000

    Added isLoading and hasErrored states to ViewProperties

--- src/components/ViewProperties.jsx
+++ src/components/ViewProperties.jsx
@@ -7,10 +7,19 @@ import { getProperties } from "../../api";
 
 export default function ViewProperties() {
   const [properties, setProperties] = useState();
+  const [isLoading, setIsLoading] = useState(true);
+  const [hasErrored, setHasErrored] = useState(null);
 
   const fetchProperties = async () => {
-    const properties = await getProperties();
-    setProperties(properties);
+    try {
+      const properties = await getProperties();
+      setProperties(properties);
+      setIsLoading(false);
+    } catch (err) {
+      setHasErrored(err);
+      console.log({ errorType: err });
+      setIsLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -20,7 +29,8 @@ export default function ViewProperties() {
   return (
     <div className="view-properties-container">
       <FilterBar />
-      <PropertyGrid properties={properties} />
+      {isLoading ? <p>Loading...</p> : <PropertyGrid properties={properties} />}
+      {hasErrored ? <p>Error: {hasErrored.message}</p> : <></>}
     </div>
   );
 }
