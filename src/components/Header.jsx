bcd07645733616b092df4a3ec77f1c01eace35f7
JHM92 <jake.murdoch@live.com>
2025-11-26 00:04:55 +0000

    Added <Link> to Header Logo which redirects to homepage.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import { Link } from "react-router";
 
 export default function Header() {
   return (
     <header className="header">
-      <div className="header-logo">
-        <h1 className="h1-header-logo">
-          <img className="logo-image" src="src/assets/airbnc-logo.png" alt="" />
-        </h1>
-      </div>
+      <Link to="/" style={{ display: "contents" }}>
+        <div className="header-logo">
+          <h1 className="h1-header-logo">
+            <img className="logo-image" src="src/assets/airbnc-logo.png" alt="" />
+          </h1>
+        </div>
+      </Link>
       <div className="header-user">
         <div className="header-welcome">
           Welcome,
