6db52e1a5d2b4fbdf153a9036bce1637e585ec65
JHM92 <jake.murdoch@live.com>
2026-01-11 23:31:10 +0000

    Updated StarRating component to use star image instead of asterisks

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,7 +1,15 @@
+import starLogo from "../assets/star.png";
 export default function StarRating({ rating }) {
-  let stars = "";
+  const stars = [];
   for (let i = 0; i < rating; i++) {
-    stars += "*";
+    stars.push("*");
   }
-  return <div className="star-rating">{stars}</div>;
+
+  return (
+    <div className="star-rating">
+      {stars.map(() => {
+        return <img className="star-filled" height="50" width="50" src={starLogo} alt="" />;
+      })}
+    </div>
+  );
 }
